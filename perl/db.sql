a51715db7f1997b77c5b12d8e99552e1a6739f31
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2021-07-26 18:25:13 +0800

    add clusters table

--- perl/db.sql
+++ perl/db.sql
@@ -23,6 +23,24 @@ cases_new int NOT NULL
 );
 
 --create table
+CREATE TABLE IF NOT EXISTS clusters (
+cluster VARCHAR(255) UNIQUE NOT NULL,
+state LONGTEXT NOT NULL,
+district LONGTEXT,
+date_announced DATE NOT NULL,
+date_last_onset DATE NOT NULL,
+category LONGTEXT,
+status LONGTEXT,
+cases_new int NOT NULL,
+cases_total int NOT NULL,
+cases_active int NOT NULL,
+tests int NOT NULL,
+icu int NOT NULL,
+deaths int NOT NULL,
+recovered int NOT NULL
+);
+
+--create table
 CREATE TABLE IF NOT EXISTS deaths_malaysia (
 date DATE PRIMARY KEY,
 deaths_new int NOT NULL
